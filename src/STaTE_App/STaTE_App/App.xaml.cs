ecd3f3bb0a04e0a53adaf8bee987714d7281852f
Kgarber21 <91912261+Kgarber21@users.noreply.github.com>
2022-09-15 10:41:25 -0400

    Initial commit of the src folder with STaTE_App

---
+++ src/STaTE_App/STaTE_App/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace STaTE_App;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ src/STaTE_App/STaTE_App/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace STaTE_App;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
