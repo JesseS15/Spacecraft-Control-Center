843b1f5312898d9fc3f712997d3b9fb2421790d9
macncheez67 <99365867+macncheez67@users.noreply.github.com>
2022-11-29 12:48:37 -0500

    Attempt

---
+++ src/SWA/tc/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserRegisterForm(UserCreationForm):
+
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length = 20)
+    last_name = forms.CharField(max_length = 20)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
--- src/SWA/tc/models.py
+++ src/SWA/tc/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 import static.testModule
+from django.contrib.auth.models import User
 
 class simulation():
     testArgument = "red"
     sim = static.testModule.testClass(testArgument)
+
+class TestConductor(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    email=models.EmailField(blank=True)
+
+    def __str__(self):
+        return self.user.username
