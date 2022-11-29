3737a41c66c69ee246d34226b0fb27c76a9019d9
macncheez67 <99365867+macncheez67@users.noreply.github.com>
2022-11-29 14:03:44 -0500

    testthis

--- src/SWA/tc/urls.py
+++ src/SWA/tc/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth
  
 urlpatterns = [
-    path('', views.login, name ='tcLogin'),
+    path('', views.Login, name ='tcLogin'),
     path('tcHome/', views.tcHome, name ='tcHome'),
     path('', auth.LogoutView.as_view(template_name ='tc/index.html'), name ='logout'),
     path('register/', views.register, name ='register'),
