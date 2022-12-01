86da6abf06575b4061b2cea625f13a6d62c5e81c
awatsummer <summer.annamaria.01@gmail.com>
2022-12-01 14:44:02 -0500

    fo files updated

--- src/SWA/fo/urls.py
+++ src/SWA/fo/urls.py
@@ -5,8 +5,10 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth
  
 urlpatterns = [
+    path('', views.Login, name ='login'),
     path('', views.Login, name ='foLogin'),
     path('foHome/', views.foHome, name ='foHome'),
+    path('foProfile/', views.foProfile, name='foProfile'),
     path('logout', views.Logout, name ='foLogout'),
-    path('register/', views.register, name ='register'),
+    path('foRegister/', views.register, name ='foRegister'),
 ]
