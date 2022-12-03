4ee3b4fe6e6545ffe36b61825e05710e525b75dc
Kgarber21 <91912261+Kgarber21@users.noreply.github.com>
2022-12-02 21:37:11 -0500

    Remove deprecated projects

--- src/DjangoFilmsCRUD-master/films/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Film, Genre
-
-
-admin.site.register(Film)
-admin.site.register(Genre)
--- src/DjangoFilmsCRUD-master/project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-
-urlpatterns = [
-    path('', include('films.urls')),
-    path('admin/', admin.site.urls),
-]
