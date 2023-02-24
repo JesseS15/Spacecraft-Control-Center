782dd0e9d436fa9d5fa20db4ec3d4c1e2c5da323
macncheez67 <99365867+macncheez67@users.noreply.github.com>
2023-02-24 00:56:37 -0500

    again for server

--- src/SWA/SWA/settings.py
+++ src/SWA/SWA/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-rq^s8ss^x#o3%+53g%pp5u37e#sf6)#x0o3z_8j#ogih7e#4st
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['108.227.182.205']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
