2ecb374c733ed9e6e0531d160ff231e8207077bb
macncheez67 <99365867+macncheez67@users.noreply.github.com>
2023-03-23 13:28:20 -0400

    fix capitals again

--- src/SWA/simapp/models.py
+++ src/SWA/simapp/models.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db import models
-from Simulation.SimObject import SimObject
-from Simulation.MissionScript import MissionScript
+from simulation.SimObject import SimObject
+from simulation.MissionScript import MissionScript
 #settings.configure()
 
 #class emptyfd(models.Model):
