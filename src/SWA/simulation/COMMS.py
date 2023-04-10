80168c3b0ad35da94cd6bd9de7058a9cd88d9cf3
awatsummer <summer.annamaria.01@gmail.com>
2023-04-10 14:59:26 -0400

    COMMS almost done

--- src/SWA/simulation/COMMS.py
+++ src/SWA/simulation/COMMS.py
@@ -4,80 +4,87 @@ import random
 class COMMS(Subsystem):
 
     checks = {
-        'signal' : random.choice([True, False]),
-        'telem' : False,
-        'processing' : False
-        }
-        
-    systemChecksList = {
-        'EPS' : False,
-        'TCS' : False,
-        'ACS' : False,
-        'COMMS' : False
-        }
-    payloadCheck = False
-    verify = False
+        "On-board Computer": True,
+        "Antenna Status": True
+    }
+
+    frequency = random.randrange(12.0, 18.0)
+    gain = random.randrange(25, 30)
+    frequencyRange = [12.0, 18.0]
+    gainRange = [25, 30]
+    allTelemetryDataGood = False
 
     def __init__(self):
         super().__init__()
-        self.gain = 1
-        self.requiredGain = random.randint(1, 10)
         print('New instance of COMMS class created')
 
-    def update():
+    def update(self):
         pass
 
-    #Comms Attribute internal commands
-    def updateGain(self, update):
-        self.gain = update
-        print("Gain has been adjusted")
+    # Main menu option 1
+    def systemChecks(self):
+        output = []
+        index = 0
+        for key in self.checks:
+            self.checks[key] = bool(random.getrandbits(1))
+            if (self.checks[key]):
+                output[index] = "The SimCrafts current " + key + "status is Reached"
+            else:
+                output[index] = "The SimCrafts current " + key + "status is not reached"
+        return output
 
-    def systemChecks(self, subsystemName, status):
-        self.systemChecksList[subsystemName] = status
-
-    #Returns for UI################################################
-    def UIReturns(self):
-        UI = {'subsystem telemetry status' : self.systemChecksList,
-              'gain' : self.gain,
-              'RPY': self.RPY,
-              'comms telem status' : self.checks['telem']
-              }
-        return UI
-
-    #Comms Console Commands #######################################
-    def verifySignalLock(self):
-        if self.checks['signal']:
-            print('Ku-Band signal is locked on')
+    # Main menu option 2
+    def verifySignal(self):
+        output = []
+        if (self.frequency < self.frequencyRange[0] or self.frequency > self.frequencyRange[1]):
+            output[0] = "The SimCrafts current signal frequency is OUTSIDE the required bandwidth of 12.000-18.000 GHz"
         else:
-            print('ERROR FOUND')
-            print('Ku-Band signal is degraded')
-            print('Atmospheric attenuation requires enhanced signal power')
-            print('Enter command "gain adjust" to reduce signal attenuation')
+            output[0] = "The SimCrafts current signal frequency is INSIDE the required bandwidth of 12.000-18.000 GHz"
+        
+        if (self.gain < self.gainRange[0] or self.gain > self.gainRange[1]):
+            output[1] = "The SimCrafts current signal gain is OUTSIDE the required strength of 25-30 dB"
+        else:
+            output[1] = "The SimCrafts current signal gain is INSIDE the required strength of 25-30 dB"
+        
+        return output
 
-    def gainAdjust(self):
-        self.checks['signal'] = True
-        print('Antenna gain adjustment commencing. Please wait...')
-        print('Ku-Band signal is transmitting nominally')
-        if not self.checks['signal']:
-            print('Ku-Band signal is unstable, please fix this before attempting')
-            return
+    # Main menu option 3
+    def signalGain(self, newGain):
+        self.gain += newGain
+        return ("Gain has changed by " + newGain + "dB")
 
-    def verifyTelemDownload(self):
-        print('Imaging download being interrogated. Please wait...')
-        print('Imaging telemtry downloaded nominally')
-        self.checks['telem'] = True
+    # Main menu option 4
+    def signalFrequency(self, newFreq):
+        self.frequency += newFreq
+        return ("Frequency has changed by " + newFreq + "GHz")
 
-    def processImageTelem(self):
-        if not self.checks['telem']:
-            print('Please verify telemetry download before attempting to process the image')
-            return
-        print('Image processing commencing. Please wait...')
-        print('Full image processing is complete')
-        self.checks['processing'] = True
+    # Main menu option 5
+    # telemetryData needs to be passed from SimObject
+    def downloadTelemetryData(self, telemetryData):
+        output = []
+        index = 1
+        self.allTelemetryDataGood
+        for key in telemetryData:
+            if telemetryData[key]:
+                output[index] = "" + key + " Telemetry...COMPLETE!"
+            else:
+                output[index] = "" + key + " Telemetry...INCOMPLETE!"
+                self.allTelemetryDataGood = False
+            index += 1
+        if (self.allTelemetryDataGood):
+            output[0] = "The Subsystem Telemetry Data has been successfully downloaded!"
+        else:
+            output[0] = "Some subsystems have not complete their missions yet and need to send their telemetry data to finish your task."
+        return output
 
+    # Main menu option 6
+    def processTelemetryData(self):
+        if self.allTelemetryDataGood:
+            return "All telemetry data has been successfully processed!"
+        else:
+            return "Some subsystems have not complete their missions yet and need to send their telemetry data to finish your task."
+
+    # Main menu option 7
     def displayImage(self):
-        if not self.checks['processing']:
-            print('Please process the image before attempting to display')
-            return
-        print('Displaying image. Please wait...')
-        #Something here that displays the image...
+        pass
+
